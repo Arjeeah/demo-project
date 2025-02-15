b6d97d306b0cb1e245e4c60048cbfe704560ce29
Arjeeah <argea2004@gmail.com>
2025-02-15 03:28:20 +0200

    after add ticket model and migriton logic

--- database/migrations/2025_02_14_213838_create_tickets_table.php
+++ database/migrations/2025_02_14_213838_create_tickets_table.php
@@ -4,23 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('purchase_date')->nullable();
+            $table->string('seat_number')->nullable();
             $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('tickets');
     }
